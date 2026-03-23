6ce3073bef338574ae3b71f786704db7732b6f25
NinkalGupta <ninkal.gupta@unolo.com>
2026-03-23 22:54:07 +0530

    Add dependency target for auto-install Apollo + Firebase

--- Package.swift
+++ Package.swift
@@ -1,27 +1,35 @@
 // swift-tools-version: 5.9
-                                                                                                                   
-  import PackageDescription
 
-  let package = Package(
-      name: "UnoloIOSSDK",
-      platforms: [
-          .iOS(.v15)
-      ],                                                                                                           
-      products: [
-          .library(                                                                                                
-              name: "UnoloIOSSDK",                                                                                 
-              targets: ["UnoloIOSSDK"]                                                                             
-          ),                                                                                                       
-      ],                                                                                                           
-      dependencies: [                                                                                              
-          .package(url: "https://github.com/apollographql/apollo-ios", exact: "0.39.0"),
-          .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.0.0"),                           
-      ],                                                                                                           
-      targets: [                                                                                                   
-          .binaryTarget(                                                                                           
-              name: "UnoloIOSSDK",                                                                                 
-              url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/0.3.0/UnoloIOSSDK.xcframework.zip",     
-              checksum: "581625dd27bbc1deb19f6bfc2c2cc90d6ef6a90090138ca208222ade80f1effd"                         
-          ),                                                                                                       
-      ]
-  )              
+import PackageDescription
+
+let package = Package(
+    name: "UnoloIOSSDK",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "UnoloIOSSDK",
+            targets: ["UnoloIOSSDK", "UnoloIOSSDKDependencies"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apollographql/apollo-ios", exact: "0.39.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.0.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "UnoloIOSSDK",
+            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/0.4.0/UnoloIOSSDK.xcframework.zip",
+            checksum: "b792eec890a5c0b3b809717ab10f46952b33fde0c1bc735785729f2a7e9989a3"
+        ),
+        .target(
+            name: "UnoloIOSSDKDependencies",
+            dependencies: [
+                .product(name: "Apollo", package: "apollo-ios"),
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+            ],
+            path: "Sources/UnoloIOSSDKDependencies"
+        ),
+    ]
+)
---
+++ Sources/UnoloIOSSDKDependencies/Empty.swift
@@ -0,0 +1,1 @@
+// This target provides required dependencies for UnoloIOSSDK
