e038ac611e62bcfacb0757a86bfbb8ab11a0500a
Ninkal <ninkal.gupta@unolo.com>
2026-03-26 11:44:06 +0530

    Update UnoloIOSSDK package version and structure
    Updated the UnoloIOSSDK package version and removed dependencies.

--- Package.swift
+++ Package.swift
@@ -1,35 +1,23 @@
-// swift-tools-version: 5.9
+  // swift-tools-version: 5.9
 
-import PackageDescription
+  import PackageDescription
 
-let package = Package(
-    name: "UnoloIOSSDK",
-    platforms: [
-        .iOS(.v15)
-    ],
-    products: [
-        .library(
-            name: "UnoloIOSSDK",
-            targets: ["UnoloIOSSDK", "UnoloIOSSDKDependencies"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apollographql/apollo-ios", exact: "0.39.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.0.0"),
-    ],
-    targets: [
-        .binaryTarget(
-            name: "UnoloIOSSDK",
-            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/0.4.1/UnoloIOSSDK.xcframework.zip",
-            checksum: "b792eec890a5c0b3b809717ab10f46952b33fde0c1bc735785729f2a7e9989a3"
-        ),
-        .target(
-            name: "UnoloIOSSDKDependencies",
-            dependencies: [
-                .product(name: "Apollo", package: "apollo-ios"),
-                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
-            ],
-            path: "Sources/UnoloIOSSDKDependencies"
-        ),
-    ]
-)
+  let package = Package(
+      name: "UnoloIOSSDK",
+      platforms: [
+          .iOS(.v15)
+      ],
+      products: [
+          .library(
+              name: "UnoloIOSSDK",
+              targets: ["UnoloIOSSDK"]
+          ),
+      ],
+      targets: [
+          .binaryTarget(
+              name: "UnoloIOSSDK",
+              url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/0.5.0/UnoloIOSSDK.xcframework.zip",
+              checksum: "5f479bf2818e1fcda29d74c8f07be55495e6bfa11963b77735f3ce5b1b37dde1"
+          ),
+      ]
+  )
