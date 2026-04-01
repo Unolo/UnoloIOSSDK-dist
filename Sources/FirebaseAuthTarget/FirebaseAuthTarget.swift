caf933cce8bfac382984ea4422e2b0114d5cd0ac
NinkalGupta <ninkal.gupta@unolo.com>
2026-04-01 14:44:57 +0530

    Force link Firebase and Apollo with symbol references, single product

--- Sources/FirebaseAuthTarget/FirebaseAuthTarget.swift
+++ Sources/FirebaseAuthTarget/FirebaseAuthTarget.swift
@@ -1,1 +1,10 @@
 @_exported import FirebaseAuth
+import FirebaseCore
+
+// Force linker to include FirebaseAuth symbols
+@available(*, unavailable)
+private enum _ForceLink {
+    static let auth: Auth.Type = Auth.self
+    static let user: User.Type = User.self
+    static let app: FirebaseApp.Type = FirebaseApp.self
+}
--- Sources/UnoloIOSSDKWrapper/UnoloIOSSDKWrapper.swift
+++ Sources/UnoloIOSSDKWrapper/UnoloIOSSDKWrapper.swift
@@ -1,1 +1,1 @@
-  @_exported import UnoloIOSSDK                                                                                   
+@_exported import UnoloIOSSDK
