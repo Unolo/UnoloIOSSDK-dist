20e415296aaf5519ee4c9cedaae1eae84441699d
NinkalGupta <ninkal.gupta@unolo.com>
2026-04-01 14:32:41 +0530

    iOS SDK v0.6.0 - Simple binaryTarget, host app manually adds Firebase + Apollo

--- Package.swift
+++ Package.swift
@@ -10,47 +10,12 @@ let package = Package(
     products: [
         .library(
             name: "UnoloIOSSDK",
-            targets: ["UnoloIOSSDKWrapper"]
+            targets: ["UnoloIOSSDK"]
         ),
-        .library(
-            name: "UnoloFirebaseAuth",
-            targets: ["FirebaseAuthTarget"]
-        ),
-        .library(
-            name: "UnoloApollo",
-            targets: ["ApolloTarget"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apollographql/apollo-ios", exact: "0.39.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.0.0"),
     ],
     targets: [
-        .target(
-            name: "UnoloIOSSDKWrapper",
-            dependencies: [
-                "UnoloIOSSDKBinary",
-                "FirebaseAuthTarget",
-                "ApolloTarget",
-            ],
-            path: "Sources/UnoloIOSSDKWrapper"
-        ),
-        .target(
-            name: "FirebaseAuthTarget",
-            dependencies: [
-                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
-            ],
-            path: "Sources/FirebaseAuthTarget"
-        ),
-        .target(
-            name: "ApolloTarget",
-            dependencies: [
-                .product(name: "Apollo", package: "apollo-ios"),
-            ],
-            path: "Sources/ApolloTarget"
-        ),
         .binaryTarget(
-            name: "UnoloIOSSDKBinary",
+            name: "UnoloIOSSDK",
             url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/0.6.0/UnoloIOSSDK.xcframework.zip",
             checksum: "a8717edf4de1a6239f784133e5a79683800d26f9eaee883b357b897756b37736"
         ),
