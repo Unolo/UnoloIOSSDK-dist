db09c2033f0359f4d82f6cd87fdc32c2b6177fe3
Ninkal <ninkal.gupta@unolo.com>
2026-04-01 12:05:31 +0530

    Update package checksum and url
    Update package checksum and url

--- Package.swift
+++ Package.swift
@@ -1,37 +1,37 @@
 // swift-tools-version: 5.9                                                                                     
-                                                                                                                  
+                  
   import PackageDescription
 
-  let package = Package(                                                                                          
-      name: "UnoloIOSSDK",                                                                                        
-      platforms: [                                                                                                
-          .iOS(.v15)                                                                                              
+  let package = Package(
+      name: "UnoloIOSSDK",
+      platforms: [
+          .iOS(.v15)
       ],                                                                                                          
-      products: [                                                                                               
-          .library(
+      products: [
+          .library(                                                                                               
               name: "UnoloIOSSDK",
               targets: ["UnoloIOSSDKWrapper"]
           ),
       ],
       dependencies: [
-          .package(url: "https://github.com/apollographql/apollo-ios", exact: "0.39.0"),                          
-          .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.0.0"),
-      ],                                                                                                          
-      targets: [                                                                                                
-          .target(                                                                                                
-              name: "UnoloIOSSDKWrapper",                                                                       
+          .package(url: "https://github.com/apollographql/apollo-ios", exact: "0.39.0"),
+          .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.0.0"),                          
+      ],
+      targets: [                                                                                                  
+          .target(
+              name: "UnoloIOSSDKWrapper",                                                                         
               dependencies: [
-                  "UnoloIOSSDKBinary",
+                  "UnoloIOSSDKBinary",                                                                            
                   .product(name: "Apollo", package: "apollo-ios"),
                   .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
               ],                                                                                                  
               path: "Sources/UnoloIOSSDKWrapper"
           ),                                                                                                      
-          .binaryTarget(                                                                                        
+          .binaryTarget(
               name: "UnoloIOSSDKBinary",
               url:
   "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/0.6.0/UnoloIOSSDK.xcframework.zip",                
               checksum: "a8717edf4de1a6239f784133e5a79683800d26f9eaee883b357b897756b37736"
           ),                                                                                                      
-      ]                                                                                                         
-  ) 
+      ]           
+  )                      
