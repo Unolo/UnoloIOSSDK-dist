3de260cfbf16bf250bc50ccd20324b1faff6fabd
Ninkal <ninkal.gupta@unolo.com>
2026-05-22 16:35:26 +0530

    Release v1.0.1 - fix internal imports leaking in .swiftinterface

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UnoloIOSSDK",
-            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.0.0/UnoloIOSSDK.xcframework.zip",
-            checksum: "26e4684e8309dffebe42a58edf6456dae0a09e201878e9ba437a310d8ebb8b13"
+            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.0.1/UnoloIOSSDK.xcframework.zip",
+            checksum: "a4ab2f7e09676051e55c90a664937bb1563189b6284476bd3036bb7f3b80903c"
         ),
     ]
 )
