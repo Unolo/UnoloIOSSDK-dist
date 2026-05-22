3d3ddfcd27738f0f11d83d0916bb1e91107ff16c
Ninkal <ninkal.gupta@unolo.com>
2026-05-22 17:21:05 +0530

    Release v1.0.3 - embed all SPM resource bundles (SVProgressHUD, Firebase, GoogleUtilities, etc.)

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UnoloIOSSDK",
-            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.0.2/UnoloIOSSDK.xcframework.zip",
-            checksum: "0d4009b95901f1d52344d59d32cd80c276ae4eea3f864c46368348c0f4bb9e3d"
+            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.0.3/UnoloIOSSDK.xcframework.zip",
+            checksum: "ff8533fcdcf17cb687d2796908579360c3b7b94ac53916288b5c6fb20a18e459"
         ),
     ]
 )
