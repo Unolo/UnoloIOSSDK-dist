1d4b770c49019732cd224c44669cc25075339bb9
NinkalGupta <ninkal.gupta@unolo.com>
2026-06-10 17:23:47 +0530

    Bump to 1.1.2: strip .private.swiftinterface so LT host builds (Apollo no longer required downstream)

--- Package.swift
+++ Package.swift
@@ -22,13 +22,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UnoloIOSSDK",
-            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.1.1/UnoloIOSSDK.xcframework.zip",
-            checksum: "b6870c45ff3224459b7a4dc3a38bc32f2cb14950215fb67f1b7568de48c2dc3b"
+            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.1.2/UnoloIOSSDK.xcframework.zip",
+            checksum: "f5849ba88e3baeed0a3f0b2ebebee10c49b7f27b1fce9390fedc21abad4e9696"
         ),
         .binaryTarget(
             name: "UnoloAttendance",
-            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.1.1/UnoloAttendance.xcframework.zip",
-            checksum: "08dcacf6b4275330b50ab9494f3e7f6366b69c08f52e0cb22353dc0987170b40"
+            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.1.2/UnoloAttendance.xcframework.zip",
+            checksum: "2ce4d8e818dd69b2fbc1700754452ac8831914a5b7ceef754752d88009a03ee2"
         ),
     ]
 )
