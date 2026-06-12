e691e81ae0b208dcf49d3adabc1331a44f4d5712
NinkalGupta <ninkal.gupta@unolo.com>
2026-06-12 17:07:00 +0530

    Bump to 1.1.12: isPunchInProgress guard prevents double punch-in (replaces mask/loader)

--- Package.swift
+++ Package.swift
@@ -22,13 +22,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UnoloIOSSDK",
-            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.1.11/UnoloIOSSDK.xcframework.zip",
-            checksum: "8d12b0c188bcfdb5017a1780a6f11757c704a7c80600a97be133207a77b45426"
+            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.1.12/UnoloIOSSDK.xcframework.zip",
+            checksum: "9e25eb818d8db98492ffc6018983501ac6b46972316ba885784942710e0f96aa"
         ),
         .binaryTarget(
             name: "UnoloAttendance",
-            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.1.11/UnoloAttendance.xcframework.zip",
-            checksum: "9770faad3b9789e05b6609a8e386ca97aac5dc452a7850f895d215ec72180b64"
+            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.1.12/UnoloAttendance.xcframework.zip",
+            checksum: "6ecb90011af573ea53e7874b18064341bacac3ad353adcc843f3605f9c80ebb3"
         ),
     ]
 )
