727bfae1d1cd4bfd089f3f425932d5ea02d5a06d
ninkal gupta <ninkal.gupta@unolo.com>
2026-09-04 14:52:43 +0530

    Release 1.0.8 — fix missing swift-tools-version line from 1.0.7

--- Package.swift
+++ Package.swift
@@ -26,17 +26,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UnoloIOSSDK",
-            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.0.7/UnoloIOSSDK.xcframework.zip",
+            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.0.8/UnoloIOSSDK.xcframework.zip",
             checksum: "c00b570b22bbd1084d88568b6e077622d3b9d137e6a7919246c42f76fb6d796c"
         ),
         .binaryTarget(
             name: "UnoloAttendance",
-            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.0.7/UnoloAttendance.xcframework.zip",
+            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.0.8/UnoloAttendance.xcframework.zip",
             checksum: "ddb33e59a196c4c9b0f656aff166c39d0864c9d3f0da5533a5d9292cebfd132b"
         ),
         .binaryTarget(
             name: "UnoloCustomClient",
-            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.0.7/UnoloCustomClient.xcframework.zip",
+            url: "https://github.com/Unolo/UnoloIOSSDK-dist/releases/download/1.0.8/UnoloCustomClient.xcframework.zip",
             checksum: "5a95887f626b531012a02e902a8db4fa60b64061338136f781e3c22a64d2eeaf"
         ),
     ]
